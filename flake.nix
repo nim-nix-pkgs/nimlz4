d829fb8f6dd261a9a49363f21df5580912afb4f8
hugosenari <hugosenari@gmail.com>
2022-03-12 04:29:35 -0300

    chore: re index nimlz4

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim wrapper for the LZ4 library. There is also a high-level API for easy use'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimlz4-master".url = "path:./master";
+  inputs."nimlz4-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimlz4-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim wrapper for the LZ4 library. There is also a high-level API for easy use'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimlz4-master.flake = false;
+  inputs.src-nimlz4-master.owner = "dfdeshom";
+  inputs.src-nimlz4-master.ref   = "refs/heads/master";
+  inputs.src-nimlz4-master.repo  = "nimlz4";
+  inputs.src-nimlz4-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimlz4-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimlz4-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
